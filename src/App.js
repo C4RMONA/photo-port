fb2fc470a25a0ae7e409eb13465595a9cbf11707
chris <christiancarmona3@gmail.com>
2022-09-01 08:06:18 -0500

    tests developed and tested

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
-// import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
